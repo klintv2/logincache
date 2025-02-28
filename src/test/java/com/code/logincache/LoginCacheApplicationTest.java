dcb58b2819d4f212f4b185e56b56c2a62d3e49b5
klintv2 <klin@tv2.dk>
2025-02-28 09:00:15 +0100

    removed unused  import

--- src/test/java/com/code/logincache/LoginCacheApplicationTest.java
+++ src/test/java/com/code/logincache/LoginCacheApplicationTest.java
@@ -6,7 +6,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import com.code.logincache.LoginService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
